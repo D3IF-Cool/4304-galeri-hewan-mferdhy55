652ae09036eefbdbfca8358a7dbb847518d61ee7
Muhammad_Ferdhy_Ananda <adniasalsabila8@gmail.com>
2021-06-05 20:23:10 +0700

    3.4.Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4041/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if4041/galerihewan/Hewan.kt
@@ -3,5 +3,5 @@ package org.d3if4041.galerihewan
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angs
 )
--- app/src/main/java/org/d3if4041/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/org/d3if4041/galerihewan/network/HewanApiService.kt
@@ -1,14 +1,20 @@
 package org.d3if4041.galerihewan.network
 
+import com.squareup.moshi.Moshi
 import org.d3if4041.galerihewan.Hewan
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://dif.indraazimi.com/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory((MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
